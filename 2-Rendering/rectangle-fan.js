9b91e97770c418c22195763de2b9460a3a98bbeb
Jeffrey Bush <jeff@coderforlife.com>
2020-08-26 00:07:45 -0400

    Adding rectangle examples.

---
+++ 2-Rendering/rectangle-fan.js
@@ -0,0 +1,92 @@
+// Renders a rectangle using a triangle fan
+'use strict';
+    
+// Global WebGL context variable
+let gl;
+
+
+// Once the document is fully loaded run this init function.
+window.addEventListener('load', function init() {
+    // Get the HTML5 canvas object from it's ID
+    const canvas = document.getElementById('webgl-canvas');
+    if (!canvas) { window.alert('Could not find #webgl-canvas'); return; }
+
+    // Get the WebGL context (save into a global variable)
+    gl = canvas.getContext('webgl2');
+    if (!gl) { window.alert("WebGL isn't available"); return; }
+
+    // Configure WebGL
+    gl.viewport(0, 0, canvas.width, canvas.height); // this is the region of the canvas we want to draw on (all of it)
+    gl.clearColor(0.0, 0.0, 1.0, 1.0); // setup the background color with red, green, blue, and alpha amounts (this is white)
+    
+    // Initialize the WebGL program and data
+    gl.program = initProgram();
+    initBuffers();
+
+    // Render the static scene
+    render();
+});
+
+
+/**
+ * Initializes the WebGL program.
+ */
+function initProgram() {
+    // Compile shaders
+    // Vertex Shader: simplest possible
+    let vert_shader = compileShader(gl, gl.VERTEX_SHADER,
+        `#version 300 es
+        precision mediump float;
+
+        in vec4 aPosition;
+        
+        void main() {
+            gl_Position = aPosition;
+        }`
+    );
+    // Fragment Shader: simplest possible, chosen color is red for each point
+    let frag_shader = compileShader(gl, gl.FRAGMENT_SHADER,
+        `#version 300 es
+        precision mediump float;
+
+        out vec4 fragColor;
+
+        void main() {
+            fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }`
+    );
+
+    // Link the shaders into a program and use them with the WebGL context
+    let program = linkProgram(gl, vert_shader, frag_shader);
+    gl.useProgram(program);
+    
+    // Get the position attribute index
+    program.aPosition = gl.getAttribLocation(program, 'aPosition'); // get the vertex shader attribute "aPosition"
+    
+    return program;
+}
+
+
+/**
+ * Initialize the data buffers.
+ */
+function initBuffers() {
+    // TODO: The vertices of the rectangle
+
+    // TODO: Load the vertex coordinate data onto the GPU
+
+    // TODO: Cleanup
+}
+
+
+/**
+ * Render the scene.
+ */
+function render() {
+    // TODO: Associate the position attribute with the position buffer
+
+    // Clear the current rendering
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    
+    // TODO: Draw the rectangle
+}
